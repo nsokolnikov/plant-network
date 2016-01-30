6b1aa7628c6e0acd707045ea1f8e4a3ecfb8fa74
Nikita Sokolnikov <nsokolnikov1995@gmail.com>
2016-01-30 13:04:08 -0800

    Json parse things that do stuf

--- app/server/socket2.js
+++ app/server/socket2.js
@@ -4,8 +4,9 @@ function MyWebSocketHandler(url, ws) {
         console.log("Connection!");
     }));
     ws.on('message', Meteor.bindEnvironment(function (msg) {
-        console.log(msg.data);
         console.log(JSON.parse(msg.data));
+        var json = JSON.parse(msg.data);
+        Measurements.add(json.time, json.plant_id, json.type, json.data);
     }));
     ws.on('close', Meteor.bindEnvironment(function () {
 
