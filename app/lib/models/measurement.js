5f7ea3231628923ace3b42ce584373ebb4498d25
Olexiy Burov <alex.burov@outlook.com>
2016-01-29 20:57:31 -0800

    Fixed some stuff

--- app/lib/models/measurement.js
+++ app/lib/models/measurement.js
@@ -1,5 +1,14 @@
 Measurements = new Mongo.Collection("measurements");
 
+Measurements.helpers({
+    add: function(time,plant_id,type,data){
+        Meteor.call('addNewMeasurement',time,plant_id,type,data);
+    },
+    getWindow: function(low,high,user_id){
+      Meteor.call('getMeasurementsForTime',low,high,user_id);
+    }
+})
+
     Meteor.methods({
         'addNewMeasurement': function (time, plant_id, type, data) {
             Measurements.insert({
@@ -13,8 +22,5 @@ Measurements = new Mongo.Collection("measurements");
 
         'getMeasurementsForTime': function (low, high, user_id) {
             return Measurements.find({user_id: user_id, time: {$gt: low, $lt: high}}, {sort: {time: -1}}).fetch();
-        },
-
-        'test': function () {
         }
     });
