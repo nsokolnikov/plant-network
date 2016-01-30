10cd1cbea4ec0db961e8c00d026342342485f752
Nikita Sokolnikov <nsokolnikov1995@gmail.com>
2016-01-30 00:02:07 -0800

    Flow routing is in and things are happening. OAuth is still broken as all zarc

---
+++ app/lib/router.js
@@ -0,0 +1,11 @@
+FlowRouter.route('/blog/:postId', {
+    action: function(params, queryParams) {
+        console.log("Yeah! We are on the post:", params.postId);
+    }
+});
+
+FlowRouter.route('/', {
+    action: function(params, queryParams){
+
+    }
+});
--- app/plant-network.js
+++ app/plant-network.js
@@ -1,3 +1,17 @@
+FlowRouter.route('/blog/:category/:postId', {
+    name: "blog",
+    action: function(params, queryParams) {
+
+    }
+});
+
+FlowRouter.route('/login/', {
+    name: "login",
+    action: function(params, queryParams){
+        console.log("login page!");
+    }
+})
+
 if (Meteor.isClient) {
 
     Template.registerHelper("debug", function (debugStuff) {
