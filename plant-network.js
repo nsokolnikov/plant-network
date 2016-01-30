81d9bb369531bcba8d57a5f615e41b4e88c7516d
Nikita Sokolnikov <nsokolnikov1995@gmail.com>
2016-01-29 17:45:02 -0800

    Todo list stuff.

--- plant-network.js
+++ plant-network.js
@@ -1,23 +1,31 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    // counter starts at 0
+    Session.setDefault('counter', 0);
+    // This code only runs on the client
+    Template.body.helpers({
+        tasks: [
+            {text: "This is task 1"},
+            {text: "This is task 2"},
+            {text: "This is task 3"}
+        ]
+    });
+    Template.hello.helpers({
+        counter: function () {
+            return Session.get('counter');
+        }
+    });
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    Template.hello.events({
+        'click button': function () {
+            // increment the counter when button is clicked
+            Session.set('counter', Session.get('counter') + 1);
+        }
+    });
 }
+Meteor.startup(function () {
+    if (Meteor.isServer) {
+    }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
+
     // code to run on server at startup
-  });
-}
+});
