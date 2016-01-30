48fe0b8dc6ff243e933a4b10840dad22617a6549
Olexiy Burov <alex.burov@outlook.com>
2016-01-29 20:31:46 -0800

    Made visible both to client and server

--- app/lib/models/measurement.js
+++ app/lib/models/measurement.js
@@ -1,16 +1,20 @@
 Measurements = new Mongo.Collection("measurements");
 
-Measurements.helpers({
-    'addNewMeasurement' : function(time,plant_id,type,data) {
-        Measurements.insert({
-            time:time,
-            plant_id:plant_id,
-            type:type,
-            data:data
-        })
-    },
+    Meteor.methods({
+        'addNewMeasurement': function (time, plant_id, type, data) {
+            Measurements.insert({
+                time: time,
+                plant_id: plant_id,
+                type: type,
+                data: data
+            })
+            console.log("Test")
+        },
 
-    'getMeasurementsForTime' : function(low,high,user_id){
-        return Measurements.find({user_id: user_id,time:{$gt:low, $lt:high}},{sort: {time: -1}}).fetch();
-    }
-});
+        'getMeasurementsForTime': function (low, high, user_id) {
+            return Measurements.find({user_id: user_id, time: {$gt: low, $lt: high}}, {sort: {time: -1}}).fetch();
+        },
+
+        'test': function () {
+        }
+    });
