a980e374e33b055e549e655fc2ddc182ad26a2de
Olexiy Burov <alex.burov@outlook.com>
2016-01-29 19:52:01 -0800

    Put code under Measurements.helpers.

--- app/lib/models/measurement.js
+++ app/lib/models/measurement.js
@@ -1,14 +1,12 @@
 Measurements = new Mongo.Collection("measurements");
 
-if (Meteor.isServer) {
-    Meteor.methods({
-        'addNewMeasurement' : function(time,plant_id,type,data) {
-            Measurements.insert({
-                time:time,
-                plant_id:plant_id,
-                type:type,
-                data:data
-            })
-        }
-    })
-}
+Measurements.helpers({
+    'addNewMeasurement' : function(time,plant_id,type,data) {
+        Measurements.insert({
+            time:time,
+            plant_id:plant_id,
+            type:type,
+            data:data
+        })
+    }
+});
